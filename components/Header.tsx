fbc24d016ca8226db10fe6fb332791fedea77b0a
yuqiang.joy <>
2024-09-16 17:48:35 +0800

    init

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import { ConnectButton } from "web3uikit";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+      <Link href="/">
+        <a>
+          <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+        </a>
+      </Link>
+      <div className="flex flex-row items-center">
+        <Link href="/">
+          <a className="mr-4 p-6">Market</a>
+        </Link>
+        <Link href="/sell-nft">
+          <a className="mr-4 p-6">Sell your NFTS</a>
+        </Link>
+        <ConnectButton moralisAuth={false} />
+      </div>
+    </nav>
+  );
+}
